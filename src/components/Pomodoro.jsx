2e1ff429659982ee9ee150201ef3c24041596eac
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-10-19 17:34:29 -0300

    Update hook state

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 import './styles/Pomodoro.scss'
 
 export default function Pomodoro (){
+
+    const [timer, setState] = useState({minute: 25, second: 0, text: '25:00', state: false});
+
+    function startTimer(event){
+        if(timer.state === false){
+            console.log(timer.state);
+            setState({ ...timer, state: true });
+        }
+
+        if(timer.state === true){
+            console.log(timer.state);
+            setState({ ...timer, state: false });
+        }
+    }
+
     return (
         <div className='pomodoro'>
             <div className='circle'>
-                <span className='counter'>25:00</span>
+                <span className='counter'>{ timer.text }</span>
                 <span className='text'>min</span>
             </div>
             <div className='controls'>
-                <button>
+                <button onClick={e => startTimer(e)}>
                     <svg className='play' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">{/*Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
                     <svg className='pause' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">{/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
                 </button>
