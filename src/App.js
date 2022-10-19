ea2158433692a753973ea1d87480a0c7ad6fe873
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-10-19 13:04:04 -0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom'
+
+import Nav from './components/Nav.jsx'
+import Home from './components/Home.jsx'
+import Pomodoro from './components/Pomodoro.jsx'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/pomodoro" element={<Pomodoro/>}/>
+      </Routes>
     </div>
   );
 }
