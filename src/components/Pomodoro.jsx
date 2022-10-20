aef1b2352810e1cdb4f924477227b7e78a9c7f0d
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-10-20 16:40:40 -0300

    Added timer hooks

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,37 +1,75 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './styles/Pomodoro.scss'
 
 export default function Pomodoro (){
+    const [timer, setState] = useState({minutes: 1, seconds: null, counter: null});
+    const [interval, intervalState] = useState({id: null, state: false});
 
-    const [timer, setState] = useState({minute: 25, second: 0, text: '25:00', state: false});
+    useEffect(()=>{
+        if(timer.seconds === null){restart()}
 
-    function startTimer(event){
-        if(timer.state === false){
-            console.log(timer.state);
-            setState({ ...timer, state: true });
+        if((timer.seconds <= 1) && (interval.state === true)){
+            clearInterval(interval.id);
+            intervalState({ id: null, state: false});
         }
+    }, [timer]);
 
-        if(timer.state === true){
-            console.log(timer.state);
-            setState({ ...timer, state: false });
+    function start(){
+
+        if(interval.state === false){
+            
+            var id = setInterval(()=>{
+
+                setState({
+                    ...timer,
+                    seconds: timer.seconds--,
+                    counter: format(timer.seconds)
+                })
+            }, 1000);
+
+            intervalState({
+                id: id,
+                state: true
+            });
+
+        }else if(interval.state === true){
+            clearInterval(interval.id);
+            intervalState({
+                id: null,
+                state: false 
+            });
         }
+    }
+    
+    function restart(){
+        if(interval.id === null){
+            setState({
+                minutes: timer.minutes,
+                seconds: timer.minutes*60,
+                counter: format(timer.minutes*60)
+            });
+        }
+    }
+
+    function format(seconds){
+        return `${('0' + Math.floor(seconds/60)).slice(-2)}:${('0' + seconds%60).slice(-2)}`
     }
 
     return (
         <div className='pomodoro'>
             <div className='circle'>
-                <span className='counter'>{ timer.text }</span>
+                <span className='counter'>{ timer.counter }</span>
                 <span className='text'>min</span>
             </div>
             <div className='controls'>
-                <button onClick={e => startTimer(e)}>
+                <button onClick={start}>
                     <svg className='play' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">{/*Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
                     <svg className='pause' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">{/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
                 </button>
 
-                <button className='restart'>
+                <button className="restart" onClick={restart}>
                     <svg className='reload' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">{/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.*/}<path d="M125.7 160H176c17.7 0 32 14.3 32 32s-14.3 32-32 32H48c-17.7 0-32-14.3-32-32V64c0-17.7 14.3-32 32-32s32 14.3 32 32v51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z"/></svg>
                 </button>
 
