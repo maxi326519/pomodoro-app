6643cad7ec90f33b7b5c8815c1de4b338299a2fc
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-10-20 22:38:53 -0300

    Update styles - Add Settings

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -2,25 +2,33 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 import './styles/Pomodoro.scss'
+import click from '../assets/audio/click.flac'
 
 export default function Pomodoro (){
     const [timer, setState] = useState({ active: false, minutes: 25, seconds: 1500, counter: '25:00'});
+    const [className, setClassname] = useState({class: ''});
+    const [display, setDisplay] = useState({ play: '', pause: 'display',  settings: '', background: 'display'});
     var id = null;
 
     function handleTime(){
         if(timer.active === false){
-            console.log('Timer activado');
+            var sound = new Audio(click);
+            sound.play();
             setState({ ...timer, active: true })
+            setClassname({ class: 'block' })
+            setDisplay({ ...display, play: 'display', pause: ''});
         }else{
-            console.log('Timer desactivado');
+            var sound = new Audio(click);
+            sound.play();
             clearTimeout(id);
             setState({ ...timer, active: false })
+            setClassname({ class: '' })
+            setDisplay({ ...display, pause: 'display', play: ''});
         }
     }
 
     function restart(){
         if(timer.active === false){
-            console.log('Timer restaurado');
             setState({
                 ...timer,
                 minutes: timer.minutes,
@@ -30,15 +38,22 @@ export default function Pomodoro (){
         }
     }
 
+    function settings(){
+        if(display.background === 'display'){
+            setDisplay({ ...display, settings: '', background: '' });
+        }else{
+            setDisplay({ ...display, settings: 'animation' });
+            setTimeout(()=>{setDisplay({ ...display, background: 'display' })}, 500)
+        }
+    }
+
     function format(seconds){
         return `${('0' + Math.floor(seconds/60)).slice(-2)}:${('0' + seconds%60).slice(-2)}`
     }
 
     useEffect(()=>{
         if(timer.active === true){
-            console.log('El timer esta activo');
             if(timer.seconds !== 0){
-                console.log('Y es diferente de 0');
                 id = setTimeout(()=>{
                     setState({
                         ...timer,
@@ -46,15 +61,7 @@ export default function Pomodoro (){
                         counter: format(timer.seconds)
                     })
                 }, 1000);
-
-                console.log(timer);
-                console.log(id);
-            }else{
-                console.log('Per es 0');
             }
-        }else{
-            console.log('El timer esta desactivado');
-            console.log(timer);
         }
     }, [timer]);
 
@@ -66,17 +73,26 @@ export default function Pomodoro (){
             </div>
             <div className='controls'>
                 <button onClick={handleTime}>
-                    <svg className='play' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">{/*Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
-                    <svg className='pause' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">{/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
+                    <svg className={display.play} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">{/*Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
+                    <svg className={display.pause} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">{/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
                 </button>
 
-                <button className="restart" onClick={restart}>
+                <button className={`restart ${className.class}`} onClick={()=>{restart(); new Audio(click).play();}}>
                     <svg className='reload' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">{/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.*/}<path d="M125.7 160H176c17.7 0 32 14.3 32 32s-14.3 32-32 32H48c-17.7 0-32-14.3-32-32V64c0-17.7 14.3-32 32-32s32 14.3 32 32v51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z"/></svg>
                 </button>
 
-                <button>
+                <button className={`${className.class}`} onClick={()=>{settings(); new Audio(click).play();}}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">{/* Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}<path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336c44.2 0 80-35.8 80-80s-35.8-80-80-80s-80 35.8-80 80s35.8 80 80 80z"/></svg>
                 </button>
+            </div>
+            <div className={`settings ${display.background}`}>
+                <div className={`settings__container ${display.settings}`}>
+                    <button onClick={settings}>x</button>
+                    <h2>SETTINGS</h2>
+                    <label>Time</label>
+                    <input type='range' step='5' min='15' max='60' onChange={(e)=>console.log(e.target.value)}/>
+                    <button onClick={settings}>Save</button>
+                </div>
             </div>
         </div>
     )
